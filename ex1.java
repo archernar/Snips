0523e3f8aeae8312e8f3226eaa5c6629553df26b
Michael Estes <michael@fastlight.com>
2020-03-11 18:58:52 -0400

    Update

---
+++ ex1.java
@@ -0,0 +1,12 @@
+/* Java Example 1
+   A very simple Java program just to show the main program structure.
+*/
+
+public class ex1
+    // Every program in Java must be embedded within a class.
+    // Every stand-alone Java application needs a main method (function).
+    public static void main (String [] args) {
+        int factor = 10;
+        System.out.println("Java is Awesome by a factor of " + factor);
+      }
+}
